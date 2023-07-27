4851acb4b06d25a335c3e7997aceb8ad91cc5501
DanielCouperthwaite <danielcoup@hotmail.co.uk>
2023-07-27 13:48:19 +0100

    added Summary class

--- Program.cs
+++ Program.cs
@@ -1,11 +1,13 @@
-﻿namespace Name
+﻿using System;
+
+namespace Name
 {
     class Program
     {
         static void Main (string[] args)
         {
             Console.WriteLine("Is it sunny today? Y/N ?");
-            string response = Console.ReadLine();
+            string? response = Console.ReadLine();
 
             if(response == "y"){
                 Yes();  
@@ -18,12 +20,16 @@
         {
             Console.WriteLine("Great!");
             Console.WriteLine("Warm enough for ice-cream? Y/N ?");
-            string response = Console.ReadLine();
+            string? response = Console.ReadLine();
 
             if(response == "y"){
                 Console.WriteLine("Nice! Better chase the ice-cream van!");
+                system.Summary a = new system.Summary("sunny and warm", "eat ice-cream");
+                a.PrintWriting();
             } else if(response == "n"){
                 Console.WriteLine("No worries, cup of tea then");;
+                system.Summary b = new system.Summary("sunny and cold", "drink a cup of tea");
+                b.PrintWriting();
             }
         }
         public static void No ()
@@ -31,12 +37,16 @@
             Console.WriteLine("No");
             Console.WriteLine("Cloudy enough for rain? Y/N ?");
 
-            string response = Console.ReadLine();
+            string? response = Console.ReadLine();
 
             if(response == "y"){
                 Console.WriteLine("Maybe grab an umbrella!");
+                system.Summary c = new system.Summary("cloudy and rainy", "use an umbrella");
+                c.PrintWriting();
             } else if(response == "n"){
                 Console.WriteLine("Perfect for a walk");
+                system.Summary d = new system.Summary("cloudy but dry", "go for a walk");
+                d.PrintWriting();
             
             }
         }
---
+++ Summary.cs
@@ -0,0 +1,23 @@
+
+namespace system
+{
+
+public class Summary
+{
+
+        public string Writing;
+
+
+        public Summary (string weather, string decision)
+        {
+            Writing = $"The weather is {weather}, You made the decision to {decision}!";
+        }
+
+        public void PrintWriting()
+        {
+            Console.WriteLine($"Summary: {Writing}");
+        }
+}
+
+}
+
