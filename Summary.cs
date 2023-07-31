a16669cb1f48d6166cf6a06676cd461ab346a13e
DanielCouperthwaite <danielcoup@hotmail.co.uk>
2023-07-31 17:51:13 +0100

    create add question method

--- Summary.cs
+++ Summary.cs
@@ -8,16 +8,22 @@ public class Summary
 {
 
         public string Writing;
+        public int QuestionsAnswered;
 
 
         public Summary (string weather, string decision)
         {
-
+            QuestionsAnswered = 0;
             Writing = $"The weather is {weather}, You made the decision to {decision}!";
         }
 
+        public void AddQuestion()
+        {
+            QuestionsAnswered++;
+        }
         public void PrintWriting()
         {
+            Console.WriteLine(QuestionsAnswered);
             Console.WriteLine($"Summary: {Writing}");
         }
 }
